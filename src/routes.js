91b7ca9708de5ee0f9ff9ffdc9206decdeb92654
Andre Magno <andremagnoribeiro@gmail.com>
2020-05-17 22:46:24 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { BrowserRouter, Router, Switch, Redirect } from "react-router-dom";
+
+const Router = () => (
+
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" componen />
+    </Switch>
+    </BrowserRouter>
+)
